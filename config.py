3b0198df7a67671d90c43e1ba78126a7411c8d2d
MonishHacker <bgmid670@gmail.com>
2024-09-01 12:17:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("6828397618:AAEV18k63fx6_Nd6Wyvd2s3_fC6a2I5WfqU")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("LegitPlayzZzYT")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("5103155981")
 
 
 
